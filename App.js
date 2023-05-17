8a150b1eaabd2fe66c253430e36339834bce6f26
yustafk <106214324+yustafkusuma@users.noreply.github.com>
2023-05-17 15:21:09 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Navigator from './src/navigators';
+import { Store } from './src/Store';
+import { Provider } from 'react-redux';
 
-export default () => <Navigator />;
-
+export default () => (
+    <Provider store={Store}>
+        <Navigator />
+    </Provider>
+);
