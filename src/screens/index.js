59024b63b78ada7f4bc14f4747f367cf578ed80f
yustafk <106214324+yustafkusuma@users.noreply.github.com>
2023-05-17 15:13:20 +0700

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,28 @@
-import SplashScreen from "./SplashScreen";
+import SplashScreen from './SplashScreen';
+import WelcomeScreen from './WelcomeScreen';
+import SigninScreen from './SigninScreen';
+import SignupScreen from './SignupScreen';
+import ForgotPasswordScreen from './ForgotPasswordScreen';
+import RegisterPhoneScreen from './RegisterPhoneScreen';
+import VerificationScreen from './VerificationScreen';
+import HomeScreen from './HomeScreen';
+import CategoryScreen from './CategoryScreen';
+import CartScreen from './CartScreen';
+import ProductScreen from './ProductScreen';
+import BookmarkScreen from './BookmarkScreen';
+import AccountScreen from './AccountScreen';
 
-export {SplashScreen};
+export {
+    SplashScreen,
+    WelcomeScreen,
+    SigninScreen,
+    SignupScreen,
+    ForgotPasswordScreen,
+    RegisterPhoneScreen,
+    VerificationScreen,
+    HomeScreen,
+    CategoryScreen,
+    CartScreen,
+    ProductScreen,
+    BookmarkScreen,
+    AccountScreen}
